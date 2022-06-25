efecb95595fa4e7e23c753965930373cc7d0eddb
Alexei Solotvinskiy <97736656+alexvile@users.noreply.github.com>
2022-06-25 21:54:47 +0300

    added render 2 type of cards and error handling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import './css/styles.css';
 import countryCardTpl from './template/country.hbs';
+import smallCountryCardTpl from './template/small-country.hbs';
 import { fetchCountries } from './js/fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from "lodash.debounce"
 
-
+Notify.init({
+    timeout: 1000,
+});
 
 const DEBOUNCE_DELAY = 300;
 
@@ -16,26 +20,64 @@ refs.search.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
 
 
 function onInputChange(e) {
-    fetchCountries(e.target.value)
-    .then(renderCountryCard)
+    if (e.target.value === '') {
+        // console.log('target empty');
+        refs.countryInfo.innerHTML = '';
+        return
+    }
+
+    fetchCountries(e.target.value.trim())
+        
+    .then(array => { 
+        
+        if (array.length > 10) {
+            // console.log('more then 10');
+            Notify.info('Too many matches found. Please enter a more specific name.');
+        }
+        else if ((array.length <= 10) && (array.length >= 2)) {
+            renderCountriesCards(array);
+        } else if (array.length === 1) {
+            renderCountryCard(array);
+        }
+        else {
+            refs.countryInfo.innerHTML = '';
+            Notify.failure('Oops, there is no country with that name');
+            // console.log('equal 0')
+        }
+    })
+    // .then(renderCountriesCards)
     .catch(onFetchError)
 }
 
-function renderCountryCard(countries) {
+function renderCountriesCards(countries) {
     // console.log(countries);
     let markup = [];
 
         countries.map(country => {
             // console.log(country)
-            console.log(countryCardTpl(country));
-            markup.push(countryCardTpl(country));
+            // console.log(countryCardTpl(country));
+            markup.push(smallCountryCardTpl(country));
         });
     // refs.countryInfo.innerHTML = markup;
     
     refs.countryInfo.innerHTML = markup.join('');
 }
 
+function renderCountryCard(countries) {
+   
+    let markup = [];
+
+        countries.map(country => {
+            markup.push(countryCardTpl(country));
+        });
+    
+    refs.countryInfo.innerHTML = markup.join('');
+    
+}
+
 function onFetchError(error) {
-    console.log('ERROR!!!!!!!!!!!!!');
+    refs.countryInfo.innerHTML = '';
+    // console.log('CATCH ERROR!!!!!!!!!!!!!');
+    Notify.failure('Oops, there is no country with that name');
 }
 
